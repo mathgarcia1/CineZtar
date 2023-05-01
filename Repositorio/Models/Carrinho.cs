c1fbb24d3b73f4a14cb67220f0d376b1d4fbb62b
Matheus Garcia <matheus_origins1401@hotmail.com>
2023-05-01 13:10:34 -0300

    Deleta tabelas
    Tabelas excluídas: Sessao, Sessao_Ingresso
    Carrinho: Removido IdSessao

--- Repositorio/Models/Carrinho.cs
+++ Repositorio/Models/Carrinho.cs
@@ -9,17 +9,13 @@ namespace Repositorio.Models
     {
         public Carrinho()
         {
-            SessaoIngressos = new HashSet<SessaoIngresso>();
         }
 
         public int IdCarrinho { get; set; }
         public int? IdUsuario { get; set; }
-        public int? IdSessao { get; set; }
         public DateTime? Data { get; set; }
         public int? QtdIngressos { get; set; }
 
-        public virtual Sessao IdSessaoNavigation { get; set; }
         public virtual Usuario IdUsuarioNavigation { get; set; }
-        public virtual ICollection<SessaoIngresso> SessaoIngressos { get; set; }
     }
 }
