d16c6c1166986e11bd5161fc1f4813653c138e05
Matheus Garcia <matheus_origins1401@hotmail.com>
2023-05-30 15:41:06 -0300

    CORS
    Added to Code, for some reason the icons of font-awesome wasnt showing anymore without this

--- Cine/Startup.cs
+++ Cine/Startup.cs
@@ -27,6 +27,21 @@ namespace Cine
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder =>
+                {
+                    builder.AllowAnyOrigin()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader();
+                });
+                options.AddPolicy("AllowLocalhost5001", builder =>
+                {
+                    builder.WithOrigins("https://localhost:5001")
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+                });
+            });
             services.AddHttpContextAccessor();
 
 
@@ -51,6 +66,7 @@ namespace Cine
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseCors();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
